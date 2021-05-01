9248cadb1e1d59423ae2e18d93c9ea51a80bb780
LNSSPsd <24724395+LNSSPsd@users.noreply.github.com>
2021-05-01 22:24:50 +0800

    Update automation

---
+++ convert.js
@@ -0,0 +1,24 @@
+const fs=require("fs");
+let gamewl=JSON.parse(fs.readFileSync("whitelist.json"));
+let config=require("./config.json");
+let wl=fs.readFileSync("whitelist.txt").toString().split("\n");
+let op=true;
+let wledusers={};
+let outwl=[];
+for(let i of gamewl){
+	wledusers[i.name]=i;
+}
+for(let i of wl){
+	if(i=="")continue;
+	if(i=="==^^Maintainers^^=="){
+		if(config.under_maintaince)break;
+		continue;
+	}
+	if(wledusers.hasOwnProperty(i)){
+		outwl.push(wledusers[i]);
+	}else{
+		outwl.push({name:i});
+	}
+	wledusers[i]={name:i};
+}
+fs.writeFileSync("whitelist.json",JSON.stringify(outwl,null,"\t"));
